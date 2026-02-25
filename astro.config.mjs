4348bba54aeca18ac12c1a7b6ed0b342eef1f572
petruzzellialessandro <petruzzellialessandro2@gmail.com>
2026-02-25 16:35:09 +0100

    config: Update Astro site URL and remove base path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://petruzzellialessandro.github.io',
-  base: 'phase-prin-website',
+  site: 'https://prin-phase.eu',
   vite: {
     plugins: [tailwindcss()]
   }
